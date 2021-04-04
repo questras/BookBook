ee4e3448940508ecefa7fa02a06f9a1e4bf52b95
martin503 <51167922+martin503@users.noreply.github.com>
2021-04-04 11:18:55 +0200

    User profile (#29)
    * username, likes added
    
    * mail, and history button added
    
    * minor view layout changes
    
    * fix for merge

--- app/app/src/main/java/com/example/bookbook/MainActivity.java
+++ app/app/src/main/java/com/example/bookbook/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.bookbook;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements NavigationHost {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container_view, new UserProfile())
+                    .setReorderingAllowed(true)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container_view, fragment)
+                .setReorderingAllowed(true);
+        if (addToBackStack)
+            transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
