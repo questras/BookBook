0e6f5af223545c470d075b41d62bb5a326f3d283
martin503 <51167922+martin503@users.noreply.github.com>
2021-04-04 19:18:38 +0200

    minor stylistic changes
    Co-Authored-By: X-R3Y <74624005+X-R3Y@users.noreply.github.com>

--- app/app/src/main/java/com/example/bookbook/NewOffer.java
+++ app/app/src/main/java/com/example/bookbook/NewOffer.java
@@ -1,15 +1,6 @@
 package com.example.bookbook;
 
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.google.android.material.button.MaterialButton;
 
 public class NewOffer extends Fragment {
 }
