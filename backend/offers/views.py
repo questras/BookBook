4f33498c6d20dd50077cbc984e4e9143c137163a
questras <michu123qwe@gmail.com>
2021-04-17 14:35:09 +0200

    add basic filtering with django-filters library

--- backend/offers/views.py
+++ backend/offers/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Offer
 from .serializers import OfferSerializer
@@ -18,6 +19,8 @@ class OfferViewSet(ModelViewSet):
     serializer_class = OfferSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsLenderOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status']
 
     def perform_create(self, serializer):
         # Set lender to be currently authenticated user.
