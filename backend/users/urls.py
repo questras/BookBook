df00f6b6b9567b6df848261b2fcc14b7916f59f4
questras <michu123qwe@gmail.com>
2021-05-02 09:30:57 +0200

    Add user profile endpoint.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
     ChangePasswordView,
+    UserProfileView,
 )
 
 urlpatterns = [
     path('change_password/', ChangePasswordView.as_view(), name='change-password'),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name='user-profile'),
 ]
